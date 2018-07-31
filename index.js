0417088a4f3ded214b6379e331758889805b670f
Fisayo Afolayan <afolayanfisayo@gmail.com>
2018-07-31 16:02:19 +0100

    ammended crptoprice url

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const pusher  = new Pusher(config)
 
 
 setInterval(() => {
-    https.get(`https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptos.coins.join()}&tsyms=${cryptos.currencies.join()}`, (response) => {
+    https.get('https://min-api.cryptocompare.com/data/pricemulti?fsyms=${cryptos.coins.join()}&tsyms=${cryptos.currencies.join()}', (response) => {
         response.setEncoding('utf8')
         .on('data', data => handleResponse(data))
         .on('error', e => console.error(e.message))
